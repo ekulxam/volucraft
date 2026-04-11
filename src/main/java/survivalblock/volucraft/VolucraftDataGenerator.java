15fe035267020b8bf3d2293bc35e6090437fe094
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2026-04-10 19:05:35 -0700

    Summary (required)

---
+++ src/main/java/survivalblock/volucraft/VolucraftDataGenerator.java
@@ -0,0 +1,11 @@
+package survivalblock.volucraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VolucraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
