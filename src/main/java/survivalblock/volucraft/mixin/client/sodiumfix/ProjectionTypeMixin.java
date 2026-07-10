cb5813e45af10260859f691d8e28510de143a414
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2026-07-09 20:30:32 -0700

    I love packing
    BlazinglyFast:tm:

--- src/main/java/survivalblock/volucraft/mixin/client/sodiumfix/ProjectionTypeMixin.java
+++ src/main/java/survivalblock/volucraft/mixin/client/sodiumfix/ProjectionTypeMixin.java
@@ -1,28 +1,40 @@
 package survivalblock.volucraft.mixin.client.sodiumfix;
 
-import com.google.common.primitives.Floats;
 import com.mojang.blaze3d.ProjectionType;
 import com.mojang.blaze3d.vertex.VertexSorting;
-import it.unimi.dsi.fastutil.ints.IntArrays;
 import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.Arrays;
 
 @Mixin(ProjectionType.class)
 public enum ProjectionTypeMixin {
     VOLUCRAFT_ORTHOGRAPHIC(
             values -> {
                 int size = values.size();
-
-                Vector3f scratch = new Vector3f();
-                float[] keys = new float[values.size()];
-                int[] indices = new int[values.size()];
-
-                for(int i = 0; i < values.size(); indices[i] = i++) {
-                    keys[i] = -values.get(i, scratch).z();
+                if (size <= 0) {
+                    return new int[0];
+                } else if (size == 1) {
+                    return new int[]{0};
                 }
 
-                IntArrays.mergeSort(indices, (o1, o2) -> Floats.compare(keys[o2], keys[o1]));
+                Vector3f scratch = new Vector3f();
+                long[] packeds = new long[size];
+                int[] indices = new int[size];
+
+                for(int i = 0; i < size; indices[i] = i++) {
+                    float scaledZ = -values.get(i, scratch).z() * 10000;
+                    packeds[i] = ((long) scaledZ << 32) | (i & 0xFFFFFFFFL);
+                }
+
+                Arrays.sort(packeds);
+
+                for (int i = 0; i < size; i++) {
+                    // yeah sorry I only needed the long for the sort, you can go now
+                    indices[i] = (int) (packeds[size - 1 - i] & 0xFFFFFFFFL);
+                }
+
                 return indices;
             },
             (matrix, bias) -> matrix.translate(0.0F, 0.0F, bias / 512.0F)
