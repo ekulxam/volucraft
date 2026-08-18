67d4bc92df190d9317681a7c190cfa8d9df8fae0
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2026-08-18 16:46:00 -0700

    Bump registrar version to 0.1.1

--- src/main/java/survivalblock/volucraft/common/init/VolucraftMenuTypes.java
+++ src/main/java/survivalblock/volucraft/common/init/VolucraftMenuTypes.java
@@ -15,12 +15,8 @@
  */
 package survivalblock.volucraft.common.init;
 
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.flag.FeatureFlags;
-import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
-import survivalblock.atmosphere.registrar.Registrant;
+import survivalblock.atmosphere.registrar.MenuTypeRegistrant;
 import survivalblock.volucraft.common.Volucraft;
 import survivalblock.volucraft.common.menu.AmalgamationMenu;
 
@@ -28,23 +24,14 @@ public final class VolucraftMenuTypes {
     private VolucraftMenuTypes() {
     }
 
-    private static final MenuTypeRegistrant REGISTRANT = new MenuTypeRegistrant(Volucraft.MOD_ID, BuiltInRegistries.MENU);
+    private static final MenuTypeRegistrant REGISTRANT = new MenuTypeRegistrant(Volucraft.MOD_ID);
 
-    @SuppressWarnings("NullableProblems")
-    public static final MenuType<AmalgamationMenu> AMALGAMATING = REGISTRANT.register("amalgamating", (MenuType.MenuSupplier<AmalgamationMenu>) AmalgamationMenu::new);
+    public static final MenuType<AmalgamationMenu> AMALGAMATING = REGISTRANT.registerSimple(
+            "amalgamating",
+            AmalgamationMenu::new
+    );
 
     public static void init() {
         // NO-OP
-    }
-
-    public static class MenuTypeRegistrant extends Registrant<MenuType<?>> {
-
-        public MenuTypeRegistrant(String modId, Registry<MenuType<?>> registry) {
-            super(modId, registry);
-        }
-
-        public <T extends AbstractContainerMenu> MenuType<T> register(String name, MenuType.MenuSupplier<T> factory) {
-            return this.register(name, new MenuType<>(factory, FeatureFlags.VANILLA_SET));
-        }
     }
 }
--- src/main/java/survivalblock/volucraft/common/init/VolucraftRecipeTypes.java
+++ src/main/java/survivalblock/volucraft/common/init/VolucraftRecipeTypes.java
@@ -15,11 +15,8 @@
  */
 package survivalblock.volucraft.common.init;
 
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeType;
-import survivalblock.atmosphere.registrar.Registrant;
+import survivalblock.atmosphere.registrar.RecipeTypeRegistrant;
 import survivalblock.volucraft.common.Volucraft;
 import survivalblock.volucraft.common.recipe.AmalgamationRecipe;
 
@@ -27,27 +24,11 @@ public final class VolucraftRecipeTypes {
     private VolucraftRecipeTypes() {
     }
 
-    private static final RecipeTypeRegistrant REGISTRANT = new RecipeTypeRegistrant(Volucraft.MOD_ID, BuiltInRegistries.RECIPE_TYPE);
+    private static final RecipeTypeRegistrant REGISTRANT = new RecipeTypeRegistrant(Volucraft.MOD_ID);
 
     public static final RecipeType<AmalgamationRecipe> AMALGAMATION = REGISTRANT.register("amalgamation");
 
     public static void init() {
         // NO-OP
-    }
-
-    public static class RecipeTypeRegistrant extends Registrant<RecipeType<?>> {
-
-        public RecipeTypeRegistrant(String modId, Registry<RecipeType<?>> registry) {
-            super(modId, registry);
-        }
-
-        public <T extends Recipe<?>> RecipeType<T> register(String name) {
-            return register(name, new RecipeType<T>() {
-                @Override
-                public String toString() {
-                    return name;
-                }
-            });
-        }
     }
 }
